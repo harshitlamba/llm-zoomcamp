017b004900dfebfb336491a637b26ef8189aa5a4
Harshit Lamba <30561954+harshitlamba@users.noreply.github.com>
2026-07-17 21:44:02 +0000

    set up files and test rag assistant

---
+++ Week05-Monitoring/assistant.py
@@ -0,0 +1,28 @@
+from ingest import load_faq_data, build_index
+from rag_helper import RAGBase
+from dotenv import load_dotenv
+from openai import OpenAI
+import os
+import sys
+
+def create_assistant(index, llm_client, model='gpt-5.4-mini'):
+    return RAGBase(index=index, llm_client=llm_client, model=model)
+
+if __name__=='__main__':
+    load_dotenv()
+    api_key = os.getenv('OPENAI_API_KEY')
+
+    llm_client = OpenAI(api_key=api_key)
+    documents = load_faq_data()
+    index = build_index(documents=documents)
+
+    assistant = create_assistant(index=index, llm_client=llm_client, model='gpt-4o-mini')
+    # set default value for query
+    query = 'How do I join the course?'
+    # allow the query value to be overridden by a command-line argument
+    if len(sys.argv) > 1:
+        query = sys.argv[1]
+    
+    answer = assistant.rag(query)
+    print(answer)
+    
